e0ea2d222570878968e5201d3e9775ab9ceadad1
alexi <alexihampson8@gmail.com>
2022-09-14 10:04:25 +0100

    starts Ticket 45

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
-const { insertUser, selectUserByName, selectAllUsers } = require("../models/users");
+const { insertUser, selectUserByName, selectAllUsers, updateUser } = require("../models/users");
+const { use } = require("../server");
 
 exports.postUser = (req, res, next) => {
   insertUser(req.body, req.file)
@@ -22,6 +23,16 @@ exports.getAllUsers = (req, res, next) => {
   selectAllUsers()
     .then((users) => {
       res.status(200).send({ users });
+    })
+    .catch(next);
+};
+
+exports.patchUserByName = (req, res, next) => {
+  const { username } = req.params;
+
+  updateUser(username, req.body, req.file)
+    .then((user) => {
+      res.status(200).send({ user });
     })
     .catch(next);
 };
